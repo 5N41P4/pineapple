01375833b566895c3184eb8d1aa1eefa6ae798b2
Aurel Corti <aurel.corti@gmail.com>
2024-05-09 17:20:40 +0200

    Changed favicon and title

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    copy({
+      targets: [
+        { src: 'public/favicon.ico', dest: 'dist/assets' }
+      ],
+      hook: 'writeBundle' // run the plugin at the end of the bundle
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
